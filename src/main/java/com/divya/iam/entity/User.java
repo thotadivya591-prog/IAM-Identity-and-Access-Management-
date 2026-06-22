8f89b67577247094cbc1c7f8061f2cbbc9c325c2
Divyeswari Devi Thota <thotadivya591@gmail.com>
2026-06-22 08:07:17 +0530

    Create initial User entity

---
+++ src/main/java/com/divya/iam/entity/User.java
@@ -0,0 +1,11 @@
+package com.divya.iam.entity;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+
+}
