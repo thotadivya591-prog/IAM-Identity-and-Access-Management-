47c7e3099618b38a50ebc2d6d38dc854f5954d41
Divyeswari Devi Thota <thotadivya591@gmail.com>
2026-07-01 19:15:01 +0530

    Add user profile model

---
+++ src/main/java/com/divya/iam/model/UserProfile.java
@@ -0,0 +1,32 @@
+package com.divya.iam.model;
+
+public class UserProfile {
+
+    private String fullName;
+    private String phoneNumber;
+    private String address;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
